f7245074c550deaf1b93e2f8ad65e4ef3cea756f
Yeison Montoya <yeisondiseno@gmail.com>
2020-11-20 21:45:25 -0500

    Y.Montoya: Init

---
+++ conceptos/entorno.js
@@ -0,0 +1,7 @@
+let nombre = process.env.NOMBRE || 'Usuario';
+let web = process.env.WEB || 'No tengo web';
+
+console.log('Hola ' + nombre );
+console.log('Mi web es: ' + web );
+
+console.log('Es un cambio');
